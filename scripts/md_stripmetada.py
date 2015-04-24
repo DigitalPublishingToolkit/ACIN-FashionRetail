43456fd0bfc6ddf597bae8ecb64e0f07c5e7b5e2
gohai <gottfried.haider@gmail.com>
2015-04-24 15:03:08 +0200

    Update md_stripmetada.py

--- scripts/md_stripmetada.py
+++ scripts/md_stripmetada.py
@@ -17,15 +17,12 @@ input_filename=os.path.abspath(sys.argv[1])
 input_file = open(input_filename, "r") # open and parse
 name = ((os.path.basename(input_filename))[:-3]).encode("ascii")
 text = (input_file.read()).decode("utf-8")
-yaml_regex = re.compile(r'(\.\.\.|\-\-\-.*\n(.*\:.*\n){1,}\.\.\.)')
+yaml_regex = re.compile(r'((^|\n)(---|\.\.\.)[\s\S]+?\n\.\.\.)')
         
 def replace(text):    
     if bool(re.match(yaml_regex, text)) == True:
         sub = re.sub(yaml_regex, '', text)
         print (sub.encode("utf-8"))
-    else:
-        print (text.encode("utf-8"))
 
-    
 replace(text)
 
